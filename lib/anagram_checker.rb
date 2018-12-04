48d10c7c844f93e95de289b349da3c402c794833
James Rodney <jlrodney95@gmail.com>
2018-12-04 22:01:56 +0000

    added instance variables

--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -1,40 +1,47 @@
 
 def main
+  puts "Please enter the word list file path"
+  file_input
   puts "Please enter the word to be checked"
   word_input
-  puts "Please enter the file path"
-  file_input
   puts "Thanks for playing!"
 end
 
 def word_input(word=gets.chomp)
-   word_sorter(word)
+  @word = word
+  input_sorter(word)
 end
 
 def file_input(file = gets.chomp)
   word_file_into_array(file)
+
 end
 
 def word_sorter(string)
-  sorted_string = string.split('').sort.join('').strip
+   return string.split('').sort.join('').strip
+end
+
+def input_sorter(word)
+  rearranged_word = word_sorter(word)
+  # matcher(rearranged_word)
 end
 
 def word_file_into_array(file)
-  word_list_array = []
+  @word_list_array = []
   File.open(file) do |f|
     f.each_line do |line|
-      word_list_array << line.split
+      @word_list_array << line.split
     end
   end
-  sort_the_arrays(word_list_array)
+  sort_the_arrays(@word_list_array)
 end
 
 def sort_the_arrays(array)
   i = 0
-  sorted_word_in_array = []
+  @sorted_word_in_array = []
   while i < array.length
-    sorted_word_in_array << word_sorter(array[i][0])
+    @sorted_word_in_array << word_sorter(array[i][0])
     i += 1
   end
-  sorted_word_in_array
+  @sorted_word_in_array
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,6 +3,6 @@ require 'anagram_checker'
 describe 'main' do
   it 'should print the instructions' do
     allow_any_instance_of(Object).to receive(:gets).and_return('hel')
-    expect{ main }.to output("Please enter the word to be checked\nPlease enter the file path\nThanks for playing!\n").to_stdout
+    expect{ main }.to output("Please enter the word list file path\nPlease enter the word to be checked\nThanks for playing!\n").to_stdout
   end
 end
