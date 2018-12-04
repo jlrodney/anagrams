5cc10b2fd76d4969711b11395280ba29b3e1bf7f
James Rodney <jlrodney95@gmail.com>
2018-12-04 15:22:12 +0000

    Word file into an array method - It converts a text file into an array of strings - This method is called from the word_input method

--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -12,6 +12,7 @@ def word_input(word=gets.chomp)
 end
 
 def file_input(file = gets.chomp)
+  word_file_into_array(file)
 end
 
 def word_splitter(string)
@@ -32,4 +33,14 @@ def combinations(array_of_arrays)
     i += 1
   end
   combos
+end
+
+def word_file_into_array(file)
+  word_list_array = []
+  File.open(file) do |f|
+    f.each_line do |line|
+      word_list_array << line.split
+    end
+  end
+  word_list_array
 end
