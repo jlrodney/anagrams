1ce62f068031fb5a29aa9187e21b2b8bf905b457
James Rodney <jlrodney95@gmail.com>
2018-12-04 11:59:54 +0000

    Testing diffrent permutations of the array - This converts the string into an array of its characters and then makes an array of arrays of all the possible permutations

--- lib/anagram_checker.rb
+++ lib/anagram_checker.rb
@@ -1,3 +1,8 @@
 def word_splitter(string)
   split_string = string.split('')
+  word_permutations(split_string)
+end
+
+def word_permutations(array)
+  permutations = array.permutation.to_a
 end
--- spec/word_splitter_spec.rb
+++ spec/word_splitter_spec.rb
@@ -2,6 +2,7 @@ require 'anagram_checker'
 
 describe 'word_splitter' do
   it 'should split the word into an array of characters' do
-    expect(word_splitter('hello')).to eq(%w[h e l l o])
+    expect(word_splitter('hel')).to eq([%w[h e l], %w[h l e], %w[e h l],
+                                        %w[e l h], %w[l h e], %w[l e h]])
   end
 end
